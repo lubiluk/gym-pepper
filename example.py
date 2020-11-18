3180a0e24909adb440ee30b45314488211c6ec2b
Pawel Gajewski <lubiluk@gmail.com>
2020-11-18 13:46:25 +0100

    timing in example

--- example.py
+++ example.py
@@ -1,14 +1,31 @@
 import gym
 import gym_pepper
+import time
+import numpy as np
 
-env = gym.make('PepperPush-v0', gui=True)
+start = time.time()
+env = gym.make('PepperPush-v0', gui=False)
+end = time.time()
+print("=== Make === {}".format(end - start))
 
+start = time.time()
 env.reset()
+end = time.time()
+print("=== Reset === {}".format(end - start))
 
-for _ in range(1000):
+start = time.time()
+for _ in range(15000):
     env.step([1.0] * 11)
+end = time.time()
+print("=== Act1 === {}".format(end - start))
 
+start = time.time()
 env.reset()
+end = time.time()
+print("=== Reset === {}".format(end - start))
 
-for _ in range(1000):
-    env.step([0.0] * 11)
+start = time.time()
+for _ in range(15000):
+    env.step(np.random.sample(11))
+end = time.time()
+print("=== Act2 === {}".format(end - start))
