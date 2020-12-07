70828e02f05e90b68b03c53f958171d9622152ad
Paweł Gajewski <lubiluk@gmail.com>
2020-12-07 23:35:01 +0100

    removed envs that don't work

--- gym_pepper/envs/pepper_push_cam.py
+++ gym_pepper/envs/pepper_push_cam.py
@@ -14,6 +14,11 @@ TIME_STEP = 0.1
 DISTANCE_THRESHOLD = 0.04
 WORKSPACE_RADIUS = 2.0
 CONTROLLABLE_JOINTS = [
+    "KneePitch",
+    "HipPitch",
+    "HipRoll",
+    "HeadYaw",
+    "HeadPitch",
     "LShoulderPitch",
     "LShoulderRoll",
     "LElbowYaw",
@@ -21,21 +26,14 @@ CONTROLLABLE_JOINTS = [
     "LWristYaw",
     "LHand",
 ]
-CONSTANT_JOINTS = [
-    "KneePitch",
-    "HipPitch",
-    "HipRoll",
-    "HeadYaw",
-    "HeadPitch",
-]
 
 
-class PepperArmPosPushEnv(gym.GoalEnv):
+class PepperPushEnv(gym.GoalEnv):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, gui=False, sim_steps_per_action=10, max_motion_speed=0.5):
         self._sim_steps = sim_steps_per_action
-        self._max_speeds = [max_motion_speed] * 6
+        self._max_speeds = [max_motion_speed] * 11
         self._gui = gui
 
         self._setup_scene()
@@ -43,7 +41,7 @@ class PepperArmPosPushEnv(gym.GoalEnv):
         self._goal_xy = self._sample_goal()
         obs = self._get_observation()
 
-        self.action_space = spaces.Box(-2.0857, 2.0857, shape=(6,), dtype='float32')
+        self.action_space = spaces.Box(-2.0857, 2.0857, shape=(11,), dtype='float32')
         self.observation_space = spaces.Dict(dict(
             desired_goal=spaces.Box(-np.inf, np.inf,
                                     shape=obs['achieved_goal'].shape, dtype='float32'),
@@ -104,34 +102,34 @@ class PepperArmPosPushEnv(gym.GoalEnv):
 
         self._robot.goToPosture("Stand", 1.0)
 
+        path = Path(__file__).parent.parent / "assets" / "models"
+        p.setAdditionalSearchPath(str(path), physicsClientId=self._client)
+
+        self._table_init_pos = [0.5, 0, 0]
+        self._table_init_ori = [0, 0, 0, 1]
+        self._obj_init_pos = [0.5, 0, 0.6]
+        self._obj_init_ori = [0, 0, 0, 1]
+
+        self._table = p.loadURDF(
+            "Lack/Lack.urdf", self._table_init_pos, self._table_init_ori, 
+            physicsClientId=self._client)
+        self._obj = p.loadURDF(
+            "cube/cube.urdf", self._obj_init_pos, self._obj_init_ori, 
+            physicsClientId=self._client)
+
+        # Let things fall down
         for _ in range(1000):
             p.stepSimulation(physicsClientId=self._client)
-
-        self._robot.setAngles(CONSTANT_JOINTS, [0.01, -1.0, 0, 0, 0,], [self._max_speed] * 5)
-        self._robot.setAngles(CONTROLLABLE_JOINTS, [0, 0, 0, -1.5, 0, 1.], [self._max_speed] * 6)
 
         self.joints_initial_pose = self._robot.getAnglesPosition(
             self._robot.joint_dict.keys())
 
-        path = Path(__file__).parent.parent / "assets" / "models"
-        p.setAdditionalSearchPath(str(path), physicsClientId=self._client)
-
-        self._lack_initial_position = [0.5, 0, 0]
-        self._lack_initial_orientation = [0, 0, 0, 1]
-        self._cube_initial_position = [0.5, 0, 0.6]
-        self._cube_initial_orientation = [0, 0, 0, 1]
-
-        self._lack = p.loadURDF(
-            "Lack/Lack.urdf", self._lack_initial_position, self._lack_initial_orientation, 
-            physicsClientId=self._client)
-        self._cube = p.loadURDF(
-            "cube/cube.urdf", self._cube_initial_position, self._cube_initial_orientation, 
-            physicsClientId=self._client)
+        self._obj_start_pos = p.getBasePositionAndOrientation(self._obj, physicsClientId=self._client)[0]
 
         if self._gui:
             # load ghosts
-            self._cube_ghost = p.loadURDF(
-                "cube/cube_ghost.urdf", self._cube_initial_position, self._cube_initial_orientation, 
+            self._ghost = p.loadURDF(
+                "cube/cube_ghost.urdf", self._obj_init_pos, self._obj_init_ori, 
                 physicsClientId=self._client, useFixedBase=True)
 
     def _reset_scene(self):
@@ -144,14 +142,14 @@ class PepperArmPosPushEnv(gym.GoalEnv):
         self._reset_joint_state()
 
         p.resetBasePositionAndOrientation(
-            self._lack,
-            posObj=self._lack_initial_position,
-            ornObj=self._lack_initial_orientation,
+            self._table,
+            posObj=self._table_init_pos,
+            ornObj=self._table_init_ori,
             physicsClientId=self._client)
         p.resetBasePositionAndOrientation(
-            self._cube,
-            posObj=self._cube_initial_position,
-            ornObj=self._cube_initial_orientation,
+            self._obj,
+            posObj=self._obj_init_pos,
+            ornObj=self._obj_init_ori,
             physicsClientId=self._client)
 
         for _ in range(self._sim_steps):
@@ -175,8 +173,8 @@ class PepperArmPosPushEnv(gym.GoalEnv):
                 physicsClientId=self._client)
 
     def _get_observation(self):
-        obj_pos = p.getBasePositionAndOrientation(self._cube, physicsClientId=self._client)[0]
-        obj_vel = p.getBaseVelocity(self._cube, physicsClientId=self._client)[0]
+        obj_pos = p.getBasePositionAndOrientation(self._obj, physicsClientId=self._client)[0]
+        obj_vel = p.getBaseVelocity(self._obj, physicsClientId=self._client)[0]
         joint_p = self._robot.getAnglesPosition(CONTROLLABLE_JOINTS)
         joint_v = self._robot.getAnglesVelocity(CONTROLLABLE_JOINTS)
         hand_idx = self._robot.link_dict["l_hand"].getIndex()
@@ -193,20 +191,19 @@ class PepperArmPosPushEnv(gym.GoalEnv):
         return np.linalg.norm(desired_goal - achieved_goal, axis=-1) < DISTANCE_THRESHOLD
 
     def _sample_goal(self):
-        return (np.random.sample(2) * [0.5, 0.5] + self._lack_initial_position[:2] - [0.25, 0.25]).astype(np.float32)
+        return (np.random.sample(2) * [0.5, 0.5] + self._table_init_pos[:2] - [0.25, 0.25]).astype(np.float32)
 
     def _is_table_displaced(self):
-        pose = p.getBasePositionAndOrientation(self._lack, physicsClientId=self._client)
+        pose = p.getBasePositionAndOrientation(self._table, physicsClientId=self._client)
         current_pose = np.array([e for t in pose for e in t], dtype=np.float32)
         desired_pose = np.concatenate(
-            [self._lack_initial_position, self._lack_initial_orientation])
+            [self._table_init_pos, self._table_init_ori])
 
         return not np.allclose(desired_pose, current_pose, atol=0.01)
 
     def _place_ghosts(self):
-        cube_pose = p.getBasePositionAndOrientation(self._cube, physicsClientId=self._client)
         p.resetBasePositionAndOrientation(
-            self._cube_ghost,
-            posObj=list(self._goal_xy) + [cube_pose[0][2]],
-            ornObj=self._cube_initial_orientation,
+            self._ghost,
+            posObj=list(self._goal_xy) + [self._obj_start_pos[2]],
+            ornObj=self._obj_init_ori,
             physicsClientId=self._client)
