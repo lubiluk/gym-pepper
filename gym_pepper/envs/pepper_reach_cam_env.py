d7a18d6d9970089c317c6d7d2172e2bf908b9439
Pawel Gajewski <lubiluk@gmail.com>
2021-02-03 14:25:19 +0100

    cam transpose

--- gym_pepper/envs/pepper_reach_cam_env.py
+++ gym_pepper/envs/pepper_reach_cam_env.py
@@ -291,7 +291,7 @@ class PepperReachCamEnv(gym.Env):
         # joint_v = self._robot.getAnglesVelocity(CONTROLLABLE_JOINTS)
 
         result = {
-            "camera_bottom": img_bottom,
+            "camera_bottom": img_bottom.transpose(2, 0, 1),
             # "joints_state": np.concatenate([joint_p, joint_v]).astype(np.float32),
             "joints_state": np.array(joint_p, dtype=np.float32)
         }
