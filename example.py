ce8291e7803a4c5311e561fea864602f3c92b64c
Pawel Gajewski <lubiluk@gmail.com>
2021-03-04 21:28:15 +0100

    test

--- example.py
+++ example.py
@@ -2,10 +2,10 @@ import gym
 import gym_pepper
 import time
 import numpy as np
-import cv2
+# import cv2
 
 start = time.time()
-env = gym.make("PepperReachCam-v0", gui=True)
+env = gym.make("PepperReach-v0", gui=True)
 end = time.time()
 print("=== Make === {}".format(end - start))
 
@@ -31,8 +31,8 @@ start = time.time()
 for _ in range(1000):
     action = np.random.sample(10) * 2 - 1
     o, r, d, i = env.step(action)
-    cv2.imshow("synthetic bottom camera", o["camera"])
-    cv2.waitKey(1)
+    # cv2.imshow("synthetic bottom camera", o["camera"])
+    # cv2.waitKey(1)
 
     if d:
         env.reset()
