0532de7c2d552865b04464fa6a344b2ff92569d4
Pawel Gajewski <lubiluk@gmail.com>
2021-03-16 17:25:19 +0100

    higher table

--- gym_pepper/envs/pepper_env.py
+++ gym_pepper/envs/pepper_env.py
@@ -111,7 +111,7 @@ class PepperEnv(gym.Env):
 
         self._table_init_pos = [0.35, 0, 0]
         self._table_init_ori = [0, 0, 0, 1]
-        self._obj_init_pos = [0.35, 0, 0.65]
+        self._obj_init_pos = [0.35, 0, 0.72]
         self._obj_init_ori = [0, 0, 0, 1]
 
         dirname = os.path.dirname(__file__)
