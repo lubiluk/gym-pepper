0f27c5cf5c612071ad1643c6913aad81d6e7ac56
Pawel Gajewski <lubiluk@gmail.com>
2021-03-26 21:28:42 +0100

    dont show

--- gym_pepper/envs/detection.py
+++ gym_pepper/envs/detection.py
@@ -9,7 +9,7 @@ def is_object_in_sight(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_range, upper_range)
 
-    cv2.imshow("camera", img)
-    cv2.imshow('mask', mask)
+    # cv2.imshow("camera", img)
+    # cv2.imshow('mask', mask)
 
     return mask.max() != 0
